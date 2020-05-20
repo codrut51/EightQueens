183a6a68d62dca32dfab6a617768ff81275166bf
Codrut Marin <codrut.marin@jamf.com>
2020-05-20 23:03:54 +0200

    Add simple logic of backtracking class + input number at runtime

--- src/main/java/com/codrutmarin/Main.java
+++ src/main/java/com/codrutmarin/Main.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Let's backtrack");
-        try{
+        try {
             Scanner scan = new Scanner(System.in);
             System.out.println("Input a number n => {2..9}: ");
             int n = scan.nextInt();
             EightQueens eq = new EightQueens(n);
             eq.compute();
-        }catch (Exception ex)
-        {
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
     }
